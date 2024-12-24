ae36435064a1a8f77d0f0ddfe04dd6443f75f213
shirokazu0502 <shinzi0502@gmail.com>
2024-12-24 15:51:49 +0900

    before LSTM_model commit

--- plot/15ch_data_plot.py
+++ plot/15ch_data_plot.py
@@ -6,11 +6,9 @@ import sys
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from sympy import plot
-
 base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(base_dir)
 from config.settings import RAW_DATA_DIR
-
 
 class CSVReader_12ch:
     def __init__(self, directory):
@@ -19,35 +17,33 @@ class CSVReader_12ch:
     def search_files(self):
         files_found = []
         for filename in os.listdir(self.directory):
-            if (
-                filename.startswith("KT") or filename.startswith("12ch")
-            ) and filename.endswith(".csv"):
+            if (filename.startswith("KT") or filename.startswith("12ch")) and filename.endswith(".csv"):
                 files_found.append(filename)
         return files_found
 
     def read_csv_file(self, filename):
         file_path = os.path.join(self.directory, filename)
-        df = pd.read_csv(file_path, header=None)
+        df = pd.read_csv(file_path,header=None)
         print(f"ファイル {filename} を読み込みました。")
         print("{}の行数は{}、列数は{}です。".format(filename, len(df), len(df.columns)))
         # 読み込んだデータフレームの操作などを行う
         # ...
         print(df)
         return df
-
+    
     def process_files(self):
         files_found = self.search_files()
         print(files_found)
         df = pd.DataFrame()
         if len(files_found) > 0:
-            df = self.read_csv_file(files_found[0])
+            df=self.read_csv_file(files_found[0])
             # df=self.header_make(df)
             print(df)
         else:
             print("指定した条件のCSVファイルは存在しません。")
             print("12ch")
         return df
-
+    
     # CSVデータ特定の箇所のデータを抽出する
     def exctract_csv_range(self, df, start_index, end_index):
         df = df[start_index:end_index]
@@ -56,7 +52,6 @@ class CSVReader_12ch:
     def find_peaks(self, df):
         peaks, _ = find_peaks(df[0], height=0)
         return peaks
-
 
 # データの読み込み
 class CSVReader_16ch:
@@ -72,23 +67,23 @@ class CSVReader_16ch:
 
     def read_csv_file(self, filename):
         file_path = os.path.join(self.directory, filename)
-        df = pd.read_csv(file_path, header=None)
+        df = pd.read_csv(file_path,header=None)
         print(f"ファイル {filename} を読み込みました。")
         print("{}の行数は{}、列数は{}です。".format(filename, len(df), len(df.columns)))
         # 読み込んだデータフレームの操作などを行う
         # ...
         return df
-
+    
     def process_files(self):
         files_found = self.search_files()
         if len(files_found) > 0:
-            df = self.read_csv_file(files_found[0])
+            df=self.read_csv_file(files_found[0])
         else:
             print("指定した条件のCSVファイルは存在しません。")
             print("12ch")
             exit()
         return df
-
+    
     # CSVデータ特定の箇所のデータを抽出する
     def exctract_csv_range(self, df, start_index, end_index):
         df = df[start_index:end_index]
@@ -97,7 +92,6 @@ class CSVReader_16ch:
     def find_peaks(self, df):
         peaks, _ = find_peaks(df[0], height=0)
         return peaks
-
 
 class DataPlotter:
     def __init__(self, df_16ch, df_12ch):
@@ -111,7 +105,8 @@ class DataPlotter:
         plt.savefig("16ch_patient3.svg", format="svg")
         plt.legend()
         plt.show()
-
+        
+    
     def plot_12ch_data(self, column):
         print(column)
         print(len(self.df_12ch[0:column]))
@@ -122,18 +117,17 @@ class DataPlotter:
         plt.legend()
         plt.show()
 
-
 def main(args):
     patient_datas_dir = args.patient_datas_dir
     normal_datas_dir = args.normal_datas_dir
-    for dir_path in [patient_datas_dir, normal_datas_dir]:
+    for dir_path in ([patient_datas_dir, normal_datas_dir]):
         csv_reader_16ch = CSVReader_16ch(dir_path)
         df_16ch = csv_reader_16ch.process_files()
         df_16ch = df_16ch.drop(17, axis=1)
         print(df_16ch)
         csv_reader_12ch = CSVReader_12ch(dir_path)
         df_12ch = csv_reader_12ch.process_files()
-        search_list = [[5, 1, -4, -5, 0, -1, -2, -3, 1, 2, -1, 1, 4, 3, -3, -1, 220]]
+        search_list=[[5, 1, -4, -5, 0, -1, -2, -3, 1, 2, -1, 1, 4, 3, -3, -1, 220]]
         # search_list=[[17430, 16952, 16799, 16748, 15324, 14738, 16094, 15870, 15265, 14769, 13534, 13868, 12025, 11946, 13721, 13161, 11]]
         # search_list=[[3, 6, 14, 10, 24, 11, 9, 11, 15, 13, 1, 4, 3, 9, 1, 3, 105]]
         search_df = pd.DataFrame(search_list)
@@ -156,12 +150,12 @@ def main(args):
         # print(target_df)
         # print("Not found")
         if dir_path == patient_datas_dir:
-            # #     df_12ch = csv_reader_12ch.exctract_csv_range(df_12ch, 753000, 755000)
-            #     for i in range(0, len(df_16ch), 100000):
-            #         extract_df_16ch = csv_reader_16ch.exctract_csv_range(df_16ch, i, i+100000)
+        # #     df_12ch = csv_reader_12ch.exctract_csv_range(df_12ch, 753000, 755000)
+        #     for i in range(0, len(df_16ch), 100000):
+        #         extract_df_16ch = csv_reader_16ch.exctract_csv_range(df_16ch, i, i+100000)
             data_plotter = DataPlotter(df_16ch, df_12ch)
             data_plotter.plot_16ch_data()
-
+            
         # peaks = csv_reader_16ch.find_peaks(df_16ch)
         if dir_path == patient_datas_dir:
             plot_column = 0
@@ -172,16 +166,21 @@ def main(args):
             # 1行目を削除
             df_12ch = df_12ch.drop(0)
             print(df_12ch["A2"])
-            # 　整数型に変更
+            #　整数型に変更
             df_12ch = df_12ch.astype(float)
         print(df_12ch.columns)
         # data_plotter.plot_12ch_data(plot_column)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--raw_data_dir", type=str, default="")
     args = parser.parse_args()
-    args.patient_datas_dir = RAW_DATA_DIR + "/patient_data/Af003_KR_3894797"
-    args.normal_datas_dir = RAW_DATA_DIR + "/takahashi_test/taniguchi/taniguchi_1107_0"
+    args.patient_datas_dir=RAW_DATA_DIR+"/patient_data/Af003_KR_3894797"
+    args.normal_datas_dir=RAW_DATA_DIR+"/takahashi_test/taniguchi/taniguchi_1107_0"
     main(args)
+  git@github.com: Permission denied (publickey).
+fatal: Could not read from remote repository.
+
+remote: Support for password authentication was removed on August 13, 2021.
+remote: Please see https://docs.github.com/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls for information on currently recommended modes of authentication.
+fatal: Authentication failed for 'https://github.com/shirokazu0502/ecg_machine_learning.git/'
